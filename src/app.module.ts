83d8d17cba08978fbed3292cc2093af6b147fd92
Strange <rivasjoaquin02@gmail.com>
2024-05-27 16:52:12 -0400

    build: add libraries build: add config module for database connection feat: add validation with class-validate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './core/auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { ProductsModule } from './products/products.module';
+import { PurchaseOrdersModule } from './purchase-orders/purchase-orders.module';
+import { SalesModule } from './sales/sales.module';
+import { QsModule } from './qs/qs.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: ConfigService.get('DB_HOST'),
+        port: ConfigService.get('DB_PORT'),
+        username: ConfigService.get('DB_USERNAME'),
+        password: ConfigService.get('DB_PASSWORD'),
+        database: ConfigService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true, //for dev only
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    SuppliersModule,
+    ProductsModule,
+    PurchaseOrdersModule,
+    SalesModule,
+    QsModule,
+    ReportsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
