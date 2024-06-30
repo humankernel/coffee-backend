6f3a97a2f91fd2654574afd6a023ddee82ad1768
Strange <rivasjoaquin02@gmail.com>
2024-06-29 22:42:55 -0400

    refactor: new fields in product refactor: improved validation of numerical fields

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -2,9 +2,12 @@ import {
   IsArray,
   IsBoolean,
   IsEnum,
+  IsInt,
   IsNumber,
   IsOptional,
   IsString,
+  Max,
+  Min,
 } from 'class-validator';
 import { ProductType } from '../entities/product.entity';
 import { Size, Temp } from '../entities/drink.entity';
@@ -21,6 +24,7 @@ export class CreateProductDto {
 
   @ApiProperty({ example: 1.5 })
   @IsNumber()
+  @Min(0)
   price: number;
 
   @ApiProperty({ enum: ProductType, example: 'drink' })
@@ -31,6 +35,26 @@ export class CreateProductDto {
   @IsOptional()
   @IsNumber()
   stock: number;
+
+  @ApiProperty({ example: 20 })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(100)
+  discount: number;
+
+  @ApiProperty({ example: 4 })
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  stars: number;
+
+  @ApiProperty({ example: 255 })
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  people: number;
 }
 
 export class FoodProductDto extends CreateProductDto {
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -22,4 +22,16 @@ export class Product {
 
   @Column({ type: 'enum', enum: ProductType })
   type: ProductType;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @Column({ type: 'float', default: 0 })
+  discount: number;
+
+  @Column({ type: 'int', default: 0 })
+  stars: number;
+
+  @Column({ type: 'int', default: 0 })
+  people: number;
 }
