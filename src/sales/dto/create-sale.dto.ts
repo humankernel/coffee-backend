608c054b4343a5e8d7c56a6afaf8ead1ead7d764
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-07-02 23:59:35 -0400

    feat: impl sales endpoint

--- src/sales/dto/create-sale.dto.ts
+++ src/sales/dto/create-sale.dto.ts
@@ -1,1 +1,39 @@
-export class CreateSaleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class CartItem {
+  @IsNotEmpty()
+  @IsInt()
+  id: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  count: number;
+}
+
+export class CreateSaleDto {
+  @ApiProperty({ example: '1' })
+  @IsInt()
+  userId: number;
+
+  @ApiProperty({ example: { id: 1, count: 3 } })
+  @ValidateNested({ each: true })
+  @Type(() => CartItem)
+  cart: { id: number; count: number }[];
+}
+
+export class SaleDto {
+  @IsInt()
+  user: number;
+
+  @IsDate()
+  createdAt: Date;
+}
